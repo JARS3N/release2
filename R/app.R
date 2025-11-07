2e008b464247b5e9c9a386a9e5258a0a644751d0
JARS3N <JARS3N@users.noreply.github.com>
2025-11-07 09:56:59 -0500

    Refactor app function to include database connection

--- R/app.R
+++ R/app.R
@@ -1,3 +1,9 @@
-app<-function(){
-shinyApp(ui = ui(), server = server())
+app <- function(con = adminKraken::con_dplyr(), table_map = TABLE_MAP) {
+  shiny::runApp(
+    shiny::shinyApp(
+      ui     = release2::ui(),                   # <- use your actual UI fn name
+      server = release2::server()  # <- and your server fn
+    ),
+    launch.browser = TRUE
+  )
 }
