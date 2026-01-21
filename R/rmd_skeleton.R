d03cf77f8fc1cd793b20861525f24440ceaa2899
JARS3N <JARS3N@users.noreply.github.com>
2026-01-21 14:25:43 -0500

    Add setup chunk to rmd_skeleton function

--- R/rmd_skeleton.R
+++ R/rmd_skeleton.R
@@ -8,7 +8,8 @@ rmd_skeleton<-function ()
   sig_section <- c(stars, codeblock, rep(blank, 3), end_tick,
                    blank)
   fl <- c(dash, "title: \"Cartridge QC Lot Release\"  ", "output: html_document  ",
-          dash, "## Lot: %lotline%", "### QC Instrument: %instline%",
+          dash, "```{r setup, include=FALSE}  ", "options(scipen = 999)  ",
+          end_tick, blank, "## Lot: %lotline%", "### QC Instrument: %instline%",
           "  ", "#### %Date%  ", "  ", "%table%", rep(blank, 3),
           "### Approved for Release by:  ", sig_section, "### Reference the following site-specific procedures when deviation from the process is applicable:",
           "#### SBS-1303-600 Procedure Deviation Process, SBS-1302-700 Form Deviation, SBS-1301-700 Request Log:",
